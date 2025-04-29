f07ba7b5a342154e00d12cfa69e75515685f70a4
beto1969 <coolmath27@hotmail.com>
2025-04-29 01:35:55 -0400

    chnaged for pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';  // you can add your own CSS
+import './index.css';
+import {BrowserRouter} from "react-router-dom";  // you can add your own CSS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter basename={"/ashley/"}>
+            <App />
+          </BrowserRouter>
   </React.StrictMode>,
 );
