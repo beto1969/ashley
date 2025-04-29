f27c15f58f2319796724e413cc6b82eff0539e90
beto1969 <coolmath27@hotmail.com>
2025-04-29 02:18:17 -0400

    chnaged for pages

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import './index.css';
 import {BrowserRouter} from "react-router-dom";  // you can add your own CSS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
   <React.StrictMode>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+
             <App />
-      </BrowserRouter>
+
   </React.StrictMode>,
+    </BrowserRouter>
 );
